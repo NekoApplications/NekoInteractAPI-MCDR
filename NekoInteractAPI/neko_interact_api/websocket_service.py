09897cad7da60bc3118ebaea43ad814c490b9f0f
SkyDynamic <tcplxw@outlook.com>
2024-03-24 14:49:07 +0800

    .

--- NekoInteractAPI/neko_interact_api/websocket_service.py
+++ NekoInteractAPI/neko_interact_api/websocket_service.py
@@ -15,8 +15,7 @@ class WebSocketService:
         self.ws = websocket.WebSocketApp(f"ws://{host}:{port}/",
                                          on_open=self.on_open,
                                          on_message=self.on_message,
-                                         on_error=self.on_error,
-                                         on_close=self.on_close)
+                                         on_error=self.on_error)
         self.ws.run_forever(ping_interval=30, ping_timeout=5, ping_payload=f"Heartbeat_PING_{time.time()}")
     
     def send(self, data: str):
@@ -32,10 +31,6 @@ class WebSocketService:
     
     def on_error(self, ws, error):
         logger.error("webSocket似乎发生了一个错误: " + str(error))
-    
-    def on_close(self, ws, close_status_code, close_msg):
-        logger.info("webSocket连接已关闭, 关闭代码: " + close_status_code)
-        self.ws = None
     
     def on_open(self, ws):
         ws.send(f"Connect_PACKET_{time.time()}")
